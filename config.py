928af8de2e880c731451f4c9ce1b006a93fbebeb
Nikola Talevski <talevskinikola1409@gmail.com>
2025-03-29 16:36:44 +0100

    Fix config

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///notesync.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_migrate import Migrate
 from app.models import db
 from app.routes import main_bp
+from config import Config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notesync.db'
+app.config.from_object(Config)
 
 db.init_app(app)
 migrate = Migrate(app, db)
